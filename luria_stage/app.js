d6e0e7a419a5bfe8ffe6c469ae56239db38318fe
maksimiliani <macseg@gmail.com>
2026-09-23 20:04:43 -0600

    upod3

--- luria_stage/app.js
+++ luria_stage/app.js
@@ -29,7 +29,7 @@ const RIVE_FILE = "./assets/luria.riv?v=20260923-1";
 const SHORT_AUDIO_FILE = "./assets/Luria-Short.mp3";
 const LONG_AUDIO_FILE = "./assets/Luria-Long.mp3";
 const BACKGROUND_AUDIO_FILE = "./assets/bg-ambient.mp3";
-const BACKGROUND_VOLUME = 0.20;
+const BACKGROUND_VOLUME = 0.08;
 
 const ARTBOARD = "Final";
 const STATE_MACHINE = "Luria State Machine";
